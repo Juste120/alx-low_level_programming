5e79e4ae9978dca26a61a1bd4461a4b0dc17318c
PAKOU Komi Juste <pakoujuste2019@gmail.com>
2025-07-05 21:16:06 +0000

    prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program,prints all numbers with putchar and
+ * 	The first 6 letters lowercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int  numbers;
+	char c;
+
+	numbers = '0';
+	while (numbers <= '9')
+	{
+		putchar(numbers);
+		numbers++;
+	}
+
+	c = 'a';
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
