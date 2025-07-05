919f4e90cbd067f54cc6882149107a21a733a926
PAKOU Komi Juste <pakoujuste2019@gmail.com>
2025-07-05 20:20:13 +0000

    print all numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program,prints all numbers with putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char numbers;
+
+	numbers = '0';
+	while (numbers <= '9')
+	{
+		putchar(numbers);
+		numbers++;
+	}
+	putchar('\n');
+	return(0);
+}
