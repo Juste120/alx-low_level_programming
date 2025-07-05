fab4cbb40546fb5d8afdca8d16191be8565ef6e1
PAKOU Komi Juste <pakoujuste2019@gmail.com>
2025-07-05 20:09:15 +0000

    correctly the betty style

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-/** 
+/**
  * main - Entry point of the program, prints the all numbers
  *
- * Return : Always 0 (Success) to indicate the program ended correctly
+ * Return : Always 0 (Success)
  */
 int main(void)
 {
